aec0e039c85f373974c2f2c58eef61d602cfb8b5
NanoniPck <picknickhermione@gmail.com>
2024-12-14 23:08:00 +0700

    can connect to ganache cli

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,18 +1,22 @@
+// NFT.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFT is ERC721 {
-    uint256 public tokenCounter;
+    uint256 public tokenCounter = 0;
 
-    constructor() ERC721("NFTMarketplace", "NFTM") { }
+    constructor() ERC721("NFTMarketplace", "NFTM") {
+        tokenCounter = 0;  // Initialize tokenCounter
+    }
 
     function mintNFT(address recipient) public returns (uint256) {
+        require(recipient != address(0), "Recipient address cannot be zero address.");
+        
         uint256 tokenId = tokenCounter;
         _safeMint(recipient, tokenId);
-        tokenCounter++;
+        tokenCounter++;  // Increment tokenCounter after minting
         return tokenId;
     }
 }
--- migrations/1_deployed_contracts.js
+++ migrations/1_deployed_contracts.js
@@ -1,13 +1,14 @@
-// Defines how contracts are deployed.
-
 const Auction = artifacts.require('Auction');
 const NFT = artifacts.require('NFT');
 
-module.exports = async function (deployer){
-    // const endInMinutes = 1;
-    // const endTime = Math.floor(Date.now() / 1000) + 60 * endInMinutes;
-    // await deployer.deploy(NFT);
-    // await deployer.deploy(Auction, NFT.address, 0, endTime);
+module.exports = async function (deployer) {
+  const endInMinutes = 1;
+  const endTime = Math.floor(Date.now() / 1000) + 60 * endInMinutes;
+
+  // Deploy NFT first and wait for it to be deployed
+  await deployer.deploy(NFT);
+  const nftInstance = await NFT.deployed();
+
+  // Now deploy Auction, passing the NFT contract address
+  await deployer.deploy(Auction, nftInstance.address, 0, endTime);
 };
-
-
